c112181ef110e17171b0c6cbf15a63163438860f
Jalles999 <jamall10j@gmail.com>
2024-11-03 23:22:20 -0300

    Add files via upload

---
+++ Semana05/Exercicio02/Exemplo2/Pessoa.cs
@@ -0,0 +1,17 @@
+namespace Exemplo1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private string sobrenome;
+        private int v;
+
+        public Pessoa(string nome, string sobrenome, int v)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.v = v;
+
+        }
+    }
+}
