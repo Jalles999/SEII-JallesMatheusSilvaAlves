53223e7619c64d2a10e7012cbf0aab1675a75640
Jalles999 <jamall10j@gmail.com>
2024-11-09 18:46:48 -0300

    Add files via upload

---
+++ Semana08/Exercicio2/Aula01/Models/BancodeDados.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud7.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public obset<Cliente> Clientes { get; set; }
+        protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"server (localdb)\mssqllocaldb;Database=Crud7; Integrated Security=True");
+        }
+    }
+}
